80ebbfa078c4e2917760389896136cac4befde5b
marcosdgr <marcos.ciruja22@gmail.com>
2025-11-06 14:58:58 -0300

    merge de ramas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
-import AdminPage from './Pages/AdminPage';
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AdminPage from "./Pages/AdminPage";
 import Footer from "./Components/Common/Footer";
 import Navbar from "./Components/Common/Navbar";
 import HomePage from "./Pages/HomePage";
@@ -10,17 +7,13 @@ import HomePage from "./Pages/HomePage";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="*" element={<div>404 - Página no encontrada</div>} />
-      </Routes>
-    </BrowserRouter>
-    <BrowserRouter>
       <div className="d-flex flex-column min-vh-100">
         <Navbar />
         <main className="flex-fill app-main">
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/admin" element={<AdminPage />} />
+            <Route path="*" element={<div>404 - Página no encontrada</div>} />
           </Routes>
         </main>
 
