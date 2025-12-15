3b6bd2944f048e9baa49034e2973238f24f1bf81
KarenLHerrera <karenherrera.dev@gmail.com>
2025-12-15 01:18:33 -0300

    se eliminan mas componentes que nos e usan

--- src/Components/Secretaria/MensajeriaInterna/MensajeriaInterna.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import MensajeriaInternaOriginal from '../../MensajeriaInterna/MensajeriaInterna';
-
-const MensajeriaSecretaria = () => {
-  return <MensajeriaInternaOriginal />;
-};
-
-export default MensajeriaSecretaria;
